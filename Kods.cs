6646df40a754ba0ca8f5a4a3784a5315f0eba47d
ASUS <ASUS@ISMAIL>
2022-11-04 10:41:13 +0300

    Add project files.

---
+++ Kods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Abstract_Member
+{
+    // Abstract üye => Sadece Abstract classlarda tanımlanabilen üyelerdir(field Hariç). polimorphsim yaklaşımını uygulamak için üyenin abstruct olarak işaretlenmesidir.
+    // abstruct üye gövdesi base tarafından değil subclass tarafından tanımlanan üyelerdir.
+    // abstruct üyeler subclaslarda override edilmek zorundadır.eğer override edilmezse hata verecektir.
+    public abstract class Insan
+    {
+        public abstract void Dusunce();
+    }
+
+    public class Patron : Insan
+    {
+        public override void Dusunce()
+        {
+            Console.WriteLine("Personel Çalışmıyor ");
+        }
+    }
+     public class Personel : Insan 
+    {
+        public override void Dusunce()
+        {
+            Console.WriteLine("Patron para vermiyor ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _6_Abstract_Member
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Personel pers = new Personel();
+            Patron pat = new Patron();
+
+
+            DusunceGoster(pers);
+            DusunceGoster(pat);
+
+            Console.ReadKey();
+
+            // ABSTRUCT ÜYE VS VİRTUAL ÜYE
+            
+            // Abstruct üyeler gövdesi base'de tanımlanmaz.
+            // Virtual üyeler gövdesi base'de tanımlanmalıdır.
+            
+            // Abstruct üyeler subclass tarafından ezilerek implement(tamamlanmak) edilmek zorundadır.
+            // Virtual üyeler subclass tarafından ezilmek zorunda değildir.
+
+        }
+        
+        static void DusunceGoster(Insan ins) 
+        {
+            ins.Dusunce();
+        }
+    }
+}
